9e6984ded9a588256ad94ea4b32f92e404df6bff
weishixin <1204353094@qq.com>
2018-12-14 16:39:42 +0800

    index.js 第一版
    数据操作还没写，用的写死的数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,17 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+var util = require('../../utils/util.js')
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    navTab: ["我的关注","与我相关","全部问题"],
+    currentNavtab: "0",
+    feed: [],
+    feed_length: 0
   },
   //事件处理函数
   bindViewTap: function() {
@@ -16,32 +20,44 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    this.refresh();
+    // if (app.globalData.userInfo) {
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   })
+    // } else if (this.data.canIUse){
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true
+    //     })
+    //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       })
+    //     }
+    //   })
+
+    // }
+
+    
+  },
+  switchTab: function (e) {
+    this.setData({
+      currentNavtab: e.currentTarget.dataset.idx
+    });
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -50,5 +66,38 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+
+//网络请求数据, 实现刷新
+  refresh0: function () {
+    var index_api = '';
+    util.getData(index_api)
+      .then(function (data) {
+        //this.setData({
+        //
+        //});
+        console.log(data);
+      });
+  },
+
+  //使用本地 fake 数据实现刷新效果
+  refresh: function () {
+    var feed = util.getDiscovery();
+    console.log("loaddata");
+    var feed_data = feed.data;
+    this.setData({
+      feed: feed_data,
+      feed_length: feed_data.length
+    });
+  },
+  //使用本地 fake 数据实现继续加载效果
+  nextLoad: function () {
+    var next = util.discoveryNext();
+    console.log("continueload");
+    var next_data = next.data;
+    this.setData({
+      feed: this.data.feed.concat(next_data),
+      feed_length: this.data.feed_length + next_data.length
+    });
   }
 })
